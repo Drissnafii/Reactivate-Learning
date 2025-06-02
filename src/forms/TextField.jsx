921c4107db8381246dc770c86cd4fecac125459e
drissnafii <drissnafi3@gmail.com>
2025-06-02 15:08:55 +0100

    fix: correct TextField component name and update usage in main

--- src/forms/TextField.jsx
+++ src/forms/TextField.jsx
@@ -1,6 +1,10 @@
-export default function Textfield(lastName) {
-    return <>
-        <label htmlFor="name">Tape Your Comment here </label>
-        <input type="text" inputNme = 'lastName'  label = 'tape ur name' />
+export default function TextField(props) {
+    console.log(props);
+    return( 
+    <>
+        <label htmlFor=""> {props.inputLabel} </label>
+        <input name= {props.inputName} type="text" />
+        <div>{props.children}</div>
     </>
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Textfield from './forms/TextField'
+import TextField from './forms/TextField'
 // import './index.css'
 // React components must start with an uppercase letter.
 // If your component is exported as default from './HelloFolder/hello', import it as 'Hello' (uppercase).
@@ -8,9 +8,18 @@ import Hello from './HelloFolder/Hello'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hello name = 'Driss' lastName = 'Nafii' />
-    <Hello name = 'Rachid' lastName = 'Nafii' />
-    <Hello name = 'Maryem' lastName = 'Nafii' />
 
-    <Textfield inputName = 'lastName' />
+    <TextField name = "firstName" inputLabel = "firstName" >
+      Veuiller saisir votre prenom...
+    </TextField>
+    
+
+    <TextField inputName = 'lastName' inputLabel = "last name" > 
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Error tempore dignissimos ratione necessitatibus pariatur
+       nulla quisquam mollitia in commodi voluptatem nesciunt,
+        minima repudiandae voluptates est eum doloribus voluptatum, 
+        reiciendis vero.
+    </TextField>
   </StrictMode>,
 )
